92159d5725a6b191befefb9af933b370a9bc1e84
MD Fardous Alom Prince <78220419+Fardous-Alom@users.noreply.github.com>
2025-04-04 20:41:23 +0600

    bug fixed netlify2

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "../styles/Dashboard.css";
-import TransactionCards from "../Components/TransactionCards";
+import TransactionCards from "../components/TransactionCards";
 import RecentTransactions from "../Components/RecentTransactions";
 import { Bar } from "react-chartjs-2";
 import {
